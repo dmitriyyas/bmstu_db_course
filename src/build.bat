55640af2c487c7b15d0d5b10e2bd3e813baccac7
Дмитрий Ясинецкий <dimaya2002@mail.ru>
2023-05-20 15:43:56 +0300

    change build script

--- src/build.bat
+++ src/build.bat
@@ -1,1 +1,11 @@
-dotnet publish --configuration Release --output .\build
+cd BL
+dotnet publish --configuration Release --output ..\build
+
+cd ..\DataAccess
+dotnet publish --configuration Release --output ..\build
+
+cd ..\MongoDbDataAccess
+dotnet publish --configuration Release --output ..\build
+
+cd ..\UI
+dotnet publish --configuration Release --output ..\build
